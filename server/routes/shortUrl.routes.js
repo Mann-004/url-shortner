ad50087458dbb8cc871114c7b5da0673987ce673
Mann <randhawamanpreet37@gmail.com>
2025-07-18 14:44:38 +0530

    changes in short url routes

--- server/routes/shortUrl.routes.js
+++ server/routes/shortUrl.routes.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { createShortUrlController } from "../controllers/shortUrl.controller.js"
+import { createShortUrlController, deleteShortUrlController } from "../controllers/shortUrl.controller.js"
 import { authMiddleware } from "../middlewares/auth.middleware.js"
 
 const router = express.Router()
 
-router.post("/shortUrl",authMiddleware,createShortUrlController)
+router.post("/create/shortUrl",authMiddleware,createShortUrlController)
+router.delete("/delete/:shortUrlId",authMiddleware,deleteShortUrlController)
 
 
 export default router
