40497222713c8313c69b7860e4d4779cbf10cfb1
Mann <randhawamanpreet37@gmail.com>
2025-07-18 21:26:53 +0530

    Routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -12,18 +12,26 @@ import HomePage from './pages/HomePage'
 import AuthPage from './pages/AuthPage'
 import ShortenPage from './pages/ShortenPage'
 import store from "./store/store"
+import ProtectedRoute from './components/ProtectedRoute'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<HomePage />}>
       <Route index element={<AuthPage />} />
-      <Route path="shorten" element={<ShortenPage />} />
+      <Route
+        path="shorten"
+        element={
+          <ProtectedRoute>
+            <ShortenPage />
+          </ProtectedRoute>
+        }
+      />
     </Route>
   )
 )
 
 createRoot(document.getElementById('root')).render(
-   <Provider store={store}>
-     <RouterProvider router={router} />
-   </Provider>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 )
